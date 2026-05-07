016c67880e3e32f197bad01f969178a2eb8c3e43
R Sunil Kumar <sunilsunilkumar80296@gmail.com>
2026-05-07 19:34:26 +0530

    main
    Updated CORS configuration to allow all origins.

--- backend/main.py
+++ backend/main.py
@@ -7,10 +7,7 @@ app = FastAPI()
 # CORS Configuration
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[
-        "http://localhost:5173",
-        "https://ai-resume-analyzer.vercel.app"
-    ],
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
